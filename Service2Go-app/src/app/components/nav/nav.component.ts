eb6b6f0023fc389790bbe31e670be68f3514a243
KinguBaka <tarikab92@yahoo.fr>
2022-12-09 14:31:29 +0100

    maj auth service : logout / login + routing

--- Service2Go-app/src/app/components/nav/nav.component.ts
+++ Service2Go-app/src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+
+  constructor(public authService:AuthService) {
+
+  }
 }
--- Service2Go-app/src/app/services/auth.service.ts
+++ Service2Go-app/src/app/services/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  isAuth:boolean;
 
-  constructor() { }
+  constructor(public router: Router) {
+    this.isAuth = false;
+
+  }
+
+  login() {
+    this.isAuth = true;
+    this.router.navigate([""])
+  }
+
+  logout() {
+    this.isAuth = false;
+    this.router.navigate([""])
+  }
 }
