9698241346362aa6103aabe2c695cb44830befae
KinguBaka <tarikab92@yahoo.fr>
2022-12-09 15:06:49 +0100

    maj service Service

--- Service2Go-app/src/app/services/service.service.ts
+++ Service2Go-app/src/app/services/service.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Service } from '../class/service';
+
+const initialService: Service[] = [
+  new Service(0,
+    {
+      categorie: "Plomberie",
+      description: "Lorem ipsum dolor"
+    },
+    {
+      categorie: "Informatique",
+      description: "Lorem ipsum dolor"
+    }
+  ),
+  new Service(1,
+    {
+      categorie: "Plomberie",
+      description: "Lorem ipsum dolor"
+    },
+    {
+      categorie: "Informatique",
+      description: "Lorem ipsum dolor"
+    }
+  )
+]
 
 @Injectable({
   providedIn: 'root'
