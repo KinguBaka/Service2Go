45a94473ab3e5b53f8a1dacf171b6e12ddd728eb
KinguBaka <tarikab92@yahoo.fr>
2022-12-09 15:41:43 +0100

    GET , POST , PUT user.service

--- Service2Go-app/src/app/services/user.service.ts
+++ Service2Go-app/src/app/services/user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '../class/user';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { catchError, Observable } from 'rxjs';
 
 const innitialUser: User[] = [
   new User("Tarik A","tarik@tarik.fr", "azerty01", false),
@@ -11,6 +13,29 @@ const innitialUser: User[] = [
   providedIn: 'root'
 })
 export class UserService {
+  private userUrl = 'api/heroes'; // API URL
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor( private http:HttpClient ) {
+
+  }
+
+  // GET one user by his id
+  getUser(id: number):Observable<User> {
+    const url = `${this.userUrl}/${id}`;
+    return this.http.get<User>(url);
+  }
+
+  // POST one user
+  addUser(user:User):Observable<User> {
+    return this.http.post<User>(this.userUrl, user, this.httpOptions);
+  }
+
+  //PUT one user
+  updateUser(user: User) {
+    return this.http.put(this.userUrl, user, this.httpOptions);
+  }
 }
