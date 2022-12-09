c5f814437d95ded15ed555a2ad823d6df73aeed4
KinguBaka <tarikab92@yahoo.fr>
2022-12-09 14:52:56 +0100

    modif service user + create class service

--- Service2Go-app/src/app/services/user.service.ts
+++ Service2Go-app/src/app/services/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@angular/core';
+import { User } from '../class/user';
+
+const innitialUser: User[] = [
+  new User("Tarik A","tarik@tarik.fr", "azerty01", false),
+  new User("Marti" , "marti@marti.fr", "azerty01", false),
+  new User("Admin", "admin@admin.fr", "azerty01", true)
+]
 
 @Injectable({
   providedIn: 'root'
