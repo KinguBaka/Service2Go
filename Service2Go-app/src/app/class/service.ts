311f5979d71efe25b97bcdecdff91b7099d538f7
KinguBaka <tarikab92@yahoo.fr>
2022-12-09 15:00:35 +0100

    maj class service

--- Service2Go-app/src/app/class/service.ts
+++ Service2Go-app/src/app/class/service.ts
@@ -1,2 +1,18 @@
 export class Service {
+
+    id:number;
+    idUser:number;
+    requestService: object;
+    givenService: object;
+    createAt:string
+    private static index:number = 0;
+
+    constructor(idUser:number, requestService:object, givenServices:object, ) {
+        this.id = Service.index++;
+        this.idUser = idUser;
+        this.requestService = requestService;
+        this.givenService = givenServices;
+        this.createAt = Date();
+    }
+
 }
