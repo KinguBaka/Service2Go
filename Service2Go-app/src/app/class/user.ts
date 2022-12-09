a3139d55d1a485d508f81d48953b59ff674760d0
KinguBaka <tarikab92@yahoo.fr>
2022-12-09 14:46:45 +0100

    maj model class user

--- Service2Go-app/src/app/class/user.ts
+++ Service2Go-app/src/app/class/user.ts
@@ -1,2 +1,17 @@
 export class User {
+
+    id:number;
+    username:string;
+    email:string;
+    password:string;
+    isAdmin:boolean;
+    private static index:number = 0;
+
+    constructor(username:string, email:string, password:string, isAdmin:boolean) {
+        this.id = User.index++;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
 }
