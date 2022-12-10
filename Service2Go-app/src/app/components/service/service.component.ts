5bc23287ad76a04322cd9902222c6b3d4c8ebc9b
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-12-10 17:16:20 +0100

    maj css add iconde home-page

--- Service2Go-app/src/app/components/service/service.component.ts
+++ Service2Go-app/src/app/components/service/service.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Service } from 'src/app/class/service';
+import { ServiceService } from 'src/app/services/service.service';
 
 @Component({
   selector: 'app-service',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./service.component.scss']
 })
 export class ServiceComponent {
+  services: Service[] = [];
 
+  constructor(private serviceService: ServiceService) {
+
+  }
+
+  ngOninit():void {
+    this.getService();
+  }
+
+  getService():void {
+    this.serviceService.getServices()
+      .subscribe(services => this.services = services);
+  }
 }
