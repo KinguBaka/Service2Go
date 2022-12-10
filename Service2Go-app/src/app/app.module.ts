bbde86b9b9911fe3c9819da74fa48dfedd38326b
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-12-10 18:10:12 +0100

    add data services

--- Service2Go-app/src/app/app.module.ts
+++ Service2Go-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { ServiceComponent } from './components/service/service.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
