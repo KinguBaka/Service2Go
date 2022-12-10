a46cce33c2115da5badb699bce5d7a5b2f3f8606
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-12-10 19:31:59 +0100

    Revert "maj css add iconde home-page"
    This reverts commit 5bc23287ad76a04322cd9902222c6b3d4c8ebc9b.

--- Service2Go-app/src/app/components/service/service.component.ts
+++ Service2Go-app/src/app/components/service/service.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Service } from 'src/app/class/service';
-import { ServiceService } from 'src/app/services/service.service';
 
 @Component({
   selector: 'app-service',
@@ -8,18 +6,5 @@ import { ServiceService } from 'src/app/services/service.service';
   styleUrls: ['./service.component.scss']
 })
 export class ServiceComponent {
-  services: Service[] = [];
 
-  constructor(private serviceService: ServiceService) {
-
-  }
-
-  ngOninit():void {
-    this.getService();
-  }
-
-  getService():void {
-    this.serviceService.getServices()
-      .subscribe(services => this.services = services);
-  }
 }
