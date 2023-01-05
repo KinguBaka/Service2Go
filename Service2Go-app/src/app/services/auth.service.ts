8e83bc0c6f1f05d8d03a84a710b29f3db8c9ee33
Tarik Aboulhouda <tarikab92@yahoo.fr>
2023-01-05 22:11:39 +0100

    add encrypt/decrypt password + environment var

--- Service2Go-app/src/app/services/auth.service.ts
+++ Service2Go-app/src/app/services/auth.service.ts
@@ -1,24 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-
+import * as CryptoJS from 'crypto-js';
+import { environment } from '../../environments/environment'
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  isAuth:boolean;
+  isAuth: boolean;
 
   constructor(public router: Router) {
     this.isAuth = false;
+  }
 
+  encrypPassword(password: any): any {
+    const encryptedPassword = CryptoJS.AES.encrypt(password, environment.secretKey);
+    return encryptedPassword;
+  }
+
+  decryptPasswrod(encryptPassword: any): string {
+    const bytes = CryptoJS.AES.decrypt(
+      encryptPassword.toString(),
+      environment.secretKey
+    );
+    const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
+    return decryptedPassword;
   }
 
   login() {
     this.isAuth = true;
-    this.router.navigate([""])
+    this.router.navigate(['']);
   }
 
   logout() {
     this.isAuth = false;
-    this.router.navigate([""])
+    this.router.navigate(['']);
   }
 }
